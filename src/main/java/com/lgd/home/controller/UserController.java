1730955275eb1e8957c5ea4fab067d604c2ccc0c
lgd <496901440@qq.com>
2019-11-17 16:28:57 +0800

    第二此一步提交哦啊

--- src/main/java/com/lgd/home/controller/UserController.java
+++ src/main/java/com/lgd/home/controller/UserController.java
@@ -6,17 +6,15 @@ import com.lgd.home.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Created by LGD on 2019/08/14.
+ * Created by LGD on 2019/08/14.111
  */
 @Controller
 @RequestMapping(value = "test")
