7637faeef196eb1e9fb17105c7c8512aba7c9680
lgd <496901440@qq.com>
2019-11-17 21:58:42 +0800

    v0.8

--- src/main/java/com/lgd/home/controller/A.java
+++ src/main/java/com/lgd/home/controller/A.java
@@ -2,5 +2,5 @@ package com.lgd.home.controller;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("this is  lgd master merge");
+        System.out.println("this is  lgd master merge 0.8");}
 }
